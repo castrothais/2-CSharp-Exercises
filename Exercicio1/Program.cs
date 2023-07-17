4eaa3eed6019ee0a3957c8d34b3e4702e4ab522b
Thais Sampaio <thais_castrocs@outlook.com>
2023-07-17 15:14:27 -0300

    feat: sum of integers

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -6,7 +6,41 @@ namespace Exercicio1
     {
         static void Main(string[] args)
         {
-            // Coloque seu código aqui
+
+            Console.WriteLine("Olá! Digite o primeiro valor: ");
+            string inputValue1 = Console.ReadLine();
+            var value1 = int.TryParse(inputValue1, out int number1);
+
+            Console.WriteLine("Digite o segundo valor: ");
+            string inputValue2 = Console.ReadLine();
+            var value2 = int.TryParse(inputValue2, out int number2);
+
+            if( inputValue1 == string.Empty || inputValue2 == string.Empty)
+            {
+                Console.WriteLine("Por favor, preencher o input corretamente!");
+
+            }
+
+            if (successfulConversation(value1, value2))
+            {
+                Console.WriteLine($"O resultado da soma de “{number1}” com “{number2}” é “{number1 + number2}”");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, ocorreu um erro durante a conversão. Tente novamente");
+            }
+
+        }
+        static bool successfulConversation(bool vaule1, bool value2)
+        {
+            if (vaule1 && value2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
